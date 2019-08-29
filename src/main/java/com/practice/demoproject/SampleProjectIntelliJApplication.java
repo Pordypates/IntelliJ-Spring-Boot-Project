df4be01ec6a818172ca43fc28a157a54310c7f3d
JYates <jordan.yates@plus.net>
2019-08-29 20:30:19 +0100

    Added Java project

---
+++ src/main/java/com/practice/demoproject/SampleProjectIntelliJApplication.java
@@ -0,0 +1,13 @@
+package com.practice.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectIntelliJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectIntelliJApplication.class, args);
+	}
+
+}
